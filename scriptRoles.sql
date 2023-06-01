c63d5a994aba22d020bd30ec7f65026489d0a855
mijx <124107747+mijx@users.noreply.github.com>
2023-05-31 22:08:07 -0500

    Update scriptRoles.sql

--- scriptRoles.sql
+++ scriptRoles.sql
@@ -54,6 +54,7 @@ GRANT SELECT ON vw_Estudiante_ver_datos_personales TO Estudiante;
 GRANT SELECT,UPDATE ON vw_Estudiante_modificar_datos_personales TO Estudiante;
 GRANT SELECT ON vw_Historia_academica TO Estudiante;
 GRANT SELECT ON vw_Horario TO Estudiante;
+GRANT SELECT ON vw_horarios TO Estudiante;
 
 DROP ROLE IF EXISTS Profesor;
 CREATE ROLE Profesor;
@@ -61,3 +62,4 @@ GRANT SELECT ON vw_Profesor_ver_datos_personales TO Profesor;
 GRANT SELECT,UPDATE ON vw_Profesor_modificar_datos_personales TO Profesor;
 GRANT SELECT ON vw_Profesor_ver_estudiante TO Profesor;
 GRANT INSERT,SELECT,UPDATE ON vw_evaluaciones_estudiante TO Profesor;
+GRANT SELECT ON vw_horarios TO Profesor;
